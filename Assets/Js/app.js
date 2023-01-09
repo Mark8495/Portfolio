12bc9a88f0ad1472b1b521841fd03bc170ac10b4
Mark8495 <markbyrne8495@gmail.com>
2023-01-09 16:49:04 +0000

    completed JS

--- Assets/Js/app.js
+++ Assets/Js/app.js
@@ -1,4 +1,13 @@
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn = document.querySelectorAll('.control');
-
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
